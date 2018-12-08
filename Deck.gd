55c31940202ae146db307ebfccce6d855a66219b
Luke Williams <luke@lukewilliamsmusic.com>
2018-12-08 14:27:50 -0500

    add card pickup & putdown sounds

--- Deck.gd
+++ Deck.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var pickupSound = load("res://audio/pickup.wav")
 
 var card = load("res://Card.tscn") # will load when the script is instanced
 var can_draw = true
@@ -16,4 +17,5 @@ func _input_event( viewport, event, shapeidx ):
 		if can_draw:
 			var card_instance = card.instance()
 			add_child(card_instance)
+			get_node("/root/Board/pickupPlayer").play(0) #play the sound!
 			can_draw = false
