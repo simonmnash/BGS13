18215d25708c5c1d3080a113dbdfc4e2f41ed15b
simonmnash <simon.m.nash@gmail.com>
2018-12-08 14:07:50 -0500

    disable cards after placement

--- Card.gd
+++ Card.gd
@@ -9,27 +9,28 @@ var card_types = ["scissiors", "rock", "paper"]
 
 func _ready():
 	$front.animation = card_types[randi() % card_types.size()]
-	self.translate(Vector2(rand_range(-100,-500),rand_range(-100,-500)))
 	pass
 
 
 
 func _unhandled_input(event):
-	if event is InputEventMouseButton and not event.pressed:
-		lifted = false
-		var areas = self.get_overlapping_areas()
-		for area in areas:
-			print(area)
-			var old_parent = self.get_parent()
-			var new_parent = area
-			old_parent.remove_child(self)
-			new_parent.add_child(self)
-			area.print_tree()
-			get_tree().get_root().find_node("Deck", true, false).reprime_draw()
-	if lifted and event is InputEventMouseMotion:
+
+
+	if lifted and not disabled and event is InputEventMouseMotion:
 		position += event.relative
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		lifted = true
-		
+		if lifted and not disabled:
+			var areas = self.get_overlapping_areas()
+			print (areas)
+			for area in areas:
+				if area.is_in_group("Slot"):
+					var old_parent = self.get_parent()
+					var new_parent = area
+					lifted = false
+					disabled = true
+					get_tree().get_root().find_node("Deck", true, false).reprime_draw()
+		else:
+			lifted = true
+			
