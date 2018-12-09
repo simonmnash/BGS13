dfeae7abfd81e26e7a23ade625e1d114d3732743
Micheal Peters <mpeters@crosswater.net>
2018-12-09 15:19:56 -0500

    Added decay pass, modified rules.

--- TurtleCity.gd
+++ TurtleCity.gd
@@ -38,19 +38,50 @@ func growth(growth_decay, wonder_tradition, order_chaos):
 					# will always decay the bottom row, since the "bottom" row is on top of an all off row. 
 					if (randi() % 2)==0:
 						self.set_cell(i, j, -1)
+						
+		rows_to_decay.invert()
+		for j in rows_to_decay:
+			for i in range(-35,145):
+				if kill_lone_cells(i, j):
+					self.set_cell(i, j, -1)
+					
+				
 
 
 # This is a placeholder CA function.
 # Two major things we want to change:
 # 1) Right now rules are static (000->0, 011->0, 111->0). Ideally we want to replace this with dyanm
 func should_turn_this_cell_on(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
+	
+	var on = false
 	# This is a little unconventional, CA convention is to use 0 as off and 1 as on, but godot uses -1 as off for a tilemap,
 	# so 0 is the first kind of tile in the tilemap.
 	
 	# c00 c10 c20
 	# c01 c11 c21
 	# c02 c12 c22
+	match wonder_tradition:
+		-3:
+			on = rule_6(i, j)
+		-2:
+			on = rule_5(i, j)
+		-1:
+			on = rule_4(i, j)
+		1:
+			on = rule_3(i, j)
+		2:
+			on = rule_2(i, j)
+		3:
+			on = rule_1(i, j)
+			
+	return on
+	
+	# THERE ARE SIX RULESETS. RULE 1 IS MOST "TRADITIONAL", RULE 6 IS MOST "WONDERFUL"
 
+func should_turn_this_cell_off(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
+
+	var off = false
+	
 	var c02 = self.get_cell(i-1, j-1)==0
 	var c12 = self.get_cell(i, j-1)==0
 	var c22 = self.get_cell(i+1, j-1)==0
@@ -59,20 +90,46 @@ func should_turn_this_cell_on(i, j, max_height, grow_decay, wonder_tradition, or
 	var c20 = self.get_cell(i+1, j+1)==0
 	var c01 = self.get_cell(i-1, j)==0
 	var c21 = self.get_cell(i+1, j)==0
-	var on = true
-
-	if not c02 and not c12 and not c22:
-		on = false
-	elif not c02 and c12 and c22:
-		on = false
-	elif c02 and c12 and c22:
-		on = false
-			
-	return on
 	
-	# THERE ARE SIX RULESETS. RULE 1 IS MOST "TRADITIONAL", RULE 6 IS MOST "WONDERFUL"
+	
+	
+	match wonder_tradition:
+		-3:
+			off = rule_6(i, j)
+		-2:
+			off = rule_5(i, j)
+		-1:
+			off = rule_4(i, j)
+		1:
+			off = rule_3(i, j)
+		2:
+			off = rule_2(i, j)
+		3:
+			off = rule_1(i, j)
+		
+	if not c02 and not c12 and not c22 and not c00 and not c10 and not c20 and not c01 and not c21:
+		off = true
+		
+	return off
 
-func rule_1(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
+func kill_lone_cells(i, j): #this should run after the decay cycle to take care of sad lonely boys
+	var off = false
+	
+	var c02 = self.get_cell(i-1, j-1)==0
+	var c12 = self.get_cell(i, j-1)==0
+	var c22 = self.get_cell(i+1, j-1)==0
+	var c00 = self.get_cell(i-1, j+1)==0
+	var c10 = self.get_cell(i, j+1)==0
+	var c20 = self.get_cell(i+1, j+1)==0
+	var c01 = self.get_cell(i-1, j)==0
+	var c21 = self.get_cell(i+1, j)==0
+	
+	if not c02 and not c12 and not c22 and not c00 and not c10 and not c20 and not c01 and not c21:
+		off = true
+		
+	return off
+
+func rule_1(i, j):
 	
 	#BUILDS A LOT! tendency to totally fill in (class 1)
 	
@@ -92,25 +149,8 @@ func rule_1(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
 		on = false
 		
 	return on
-	
 
-func should_turn_this_cell_off(i, j, max_height, grow_decay, wonder_traditions, order_chaos):
-	var c02 = self.get_cell(i-1, j-1)==0
-	var c12 = self.get_cell(i, j-1)==0
-	var c22 = self.get_cell(i+1, j-1)==0
-	var c00 = self.get_cell(i-1, j+1)==0
-	var c10 = self.get_cell(i, j+1)==0
-	var c20 = self.get_cell(i+1, j+1)==0
-	var c01 = self.get_cell(i-1, j)==0
-	var c21 = self.get_cell(i+1, j)==0
-	
-	var off = false
-	
-	if not c12:
-		off = true
-	return off
-
-func rule_2(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
+func rule_2(i, j):
 	
 	#BUILDS STRAIGHT UP (class 2)
 	
@@ -135,7 +175,7 @@ func rule_2(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
 		
 	return on
 
-func rule_3(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
+func rule_3(i, j):
 	
 	#BUILDS SOME NICE DIAGONALS (class 2)
 	
@@ -166,7 +206,7 @@ func rule_3(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
 		
 	return on
 
-func rule_4(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
+func rule_4(i, j):
 	
 	#THIS ONE MAKES PYRAMID-LIKE THINGS (class 3)
 	
@@ -196,7 +236,7 @@ func rule_4(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
 	return on
 	
 
-func rule_5(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
+func rule_5(i, j):
 	
 	#THIS ONE MAKES COOL RANDOM-ISH TRIANGLE-Y STUFF (RULE 30, class 3)
 	
@@ -227,7 +267,7 @@ func rule_5(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
 		
 	return on
 
-func rule_6(i, j, max_height, grow_decay, wonder_tradition, order_chaos):
+func rule_6(i, j):
 	
 	#SERPINSKI! (rule 90, class 4)
 	
