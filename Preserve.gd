010fdda4d015beede3ec1fc17cd0f98592d59342
simonmnash <simon.m.nash@gmail.com>
2018-12-09 18:25:09 -0500

    fixes

--- Preserve.gd
+++ Preserve.gd
@@ -11,15 +11,15 @@ func _ready():
 
 func _input_event( viewport, event, shapeidx ):
 	if (event is InputEventMouseButton && event.pressed):
-		var cards_on_board = get_tree().get_nodes_in_group("Card")
-		for card in cards_on_board:
-			card.queue_free()
-		get_tree().get_root().find_node("TurtleCity", true, false).queue_free()
-		get_tree().get_root().find_node("WonderTraditionSlot", true, false).queue_free()
-		get_tree().get_root().find_node("GrowthDecaySlot", true, false).queue_free()
-		get_tree().get_root().find_node("OrderChaosSlot", true, false).queue_free()
-		get_tree().get_root().find_node("deck", true, false).queue_free()
-		get_tree().get_root().find_node("Fortell", true, false).queue_free()
-		get_tree().get_root().find_node("Deck", true, false).queue_free()
-		self.queue_free()
-		emit_signal("victory")
+		if get_tree().get_root().find_node("Deck", true, false).can_draw:
+			var cards_on_board = get_tree().get_nodes_in_group("Card")
+			for card in cards_on_board:
+				card.queue_free()
+			get_tree().get_root().find_node("WonderTraditionSlot", true, false).queue_free()
+			get_tree().get_root().find_node("GrowthDecaySlot", true, false).queue_free()
+			get_tree().get_root().find_node("OrderChaosSlot", true, false).queue_free()
+			get_tree().get_root().find_node("deck", true, false).queue_free()
+			get_tree().get_root().find_node("Fortell", true, false).queue_free()
+			get_tree().get_root().find_node("Deck", true, false).queue_free()
+			self.queue_free()
+			emit_signal("victory")
